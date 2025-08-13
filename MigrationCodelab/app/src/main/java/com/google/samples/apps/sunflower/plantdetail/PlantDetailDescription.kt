8c1d2a046147259c8ec7cd2da3ca23e18d82e6fd
muham <muhambriana.31@gmail.com>
2025-08-13 17:11:09 +0700

    day1: More XML code migration

--- MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -17,6 +17,7 @@
 package com.google.samples.apps.sunflower.plantdetail
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
@@ -29,8 +30,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.pluralStringResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
@@ -40,21 +45,24 @@ import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
 fun PlantDetailDescription(
     plantDetailViewModel: PlantDetailViewModel
 ) {
-    val isDark = isSystemInDarkTheme()
-    MaterialTheme(
-        colorScheme = if (isDark) darkColorScheme() else lightColorScheme()
-    ) {
-        val plant by plantDetailViewModel.plant.observeAsState()
+    val plant by plantDetailViewModel.plant.observeAsState()
 
-        plant?.let {
-            PlantDetailContent(it)
-        }
+    plant?.let {
+        PlantDetailContent(it)
     }
 }
 
 @Composable
 fun PlantDetailContent(plant: Plant) {
-    PlantName(plant.name)
+    val isDark = isSystemInDarkTheme()
+    MaterialTheme(
+        colorScheme = if (isDark) darkColorScheme() else lightColorScheme()
+    ) {
+        Column(Modifier.padding(dimensionResource(R.dimen.margin_normal))) {
+            PlantName(plant.name)
+            PlantWatering(plant.wateringInterval)
+        }
+    }
 }
 
 @Composable
@@ -80,5 +88,40 @@ private  fun PlantNamePreview() {
     ) {
         val plant = Plant("id", "Apple", "description", 3, 30, "")
         PlantDetailContent(plant)
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+private fun PlantWatering(wateringInterval: Int) {
+    Column(Modifier.fillMaxWidth()) {
+        val centerWithPaddingModifier = Modifier
+            .padding(horizontal = dimensionResource(R.dimen.margin_small))
+            .align(Alignment.CenterHorizontally)
+
+        val normalPadding = dimensionResource(R.dimen.margin_normal)
+
+        Text(
+            text = stringResource(R.string.watering_needs_prefix),
+            color = MaterialTheme.colorScheme.primaryContainer,
+            fontWeight = FontWeight.Bold,
+            modifier = centerWithPaddingModifier.padding(top = normalPadding)
+        )
+
+        val wateringIntervalText = pluralStringResource(
+            R.plurals.watering_needs_suffix, wateringInterval, wateringInterval
+        )
+        Text(
+            text = wateringIntervalText,
+            modifier = centerWithPaddingModifier,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PlantWateringPreview() {
+    MaterialTheme {
+        PlantWatering(7)
     }
 }
